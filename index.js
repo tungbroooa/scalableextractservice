eed3c29839d31a5fb73e592286ace2feda89d02b
tungbroooa <lisa.maronpun0suy@hotmail.com>
2024-05-11 04:53:42 +0700

    First commit

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function findKthLargest(nums, k) {
-  nums.sort((a, b) => b - a);
-  return nums[k - 1];
+function searchMatrix(matrix, target) {
+  if (matrix.length === 0 || matrix[0].length === 0) return false;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  let row = 0;
+  let col = cols - 1;
+  while (row < rows && col >= 0) {
+    if (matrix[row][col] === target) return true;
+    else if (matrix[row][col] < target) row++;
+    else col--;
+  }
+  return false;
 }
