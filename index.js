7e87ccb5bd8a7b73d1956c068e10c7965e4e714c
tungbroooa <lisa.maronpun0suy@hotmail.com>
2024-05-13 12:32:03 +0700

    demo

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-function numDecodings(s) {
-  const dp = new Array(s.length + 1).fill(0);
-  dp[0] = 1;
-  dp[1] = s[0] === "0" ? 0 : 1;
-  for (let i = 2; i <= s.length; i++) {
-    const oneDigit = parseInt(s.substring(i - 1, i));
-    const twoDigits = parseInt(s.substring(i - 2, i));
-    if (oneDigit >= 1) {
-      dp[i] += dp[i - 1];
-    }
-    if (twoDigits >= 10 && twoDigits <= 26) {
-      dp[i] += dp[i - 2];
+const linearSearch = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
     }
   }
-  return dp[s.length];
-}
+  return -1;
+};
